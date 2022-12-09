5056eefa58da93c275dbbda51857c55b0ea4f60d
AnnahitaSarre <asarre00@gmail.com>
2022-12-09 16:33:33 +0100

    rerunned pipeline + modified some lpc files

--- code/predict_and_mark.sh
+++ code/predict_and_mark.sh
@@ -3,10 +3,10 @@
 #SBATCH --time=01:00:00
 #SBATCH --mem=10G
 #SBATCH --cpus-per-task=1
-#SBATCH --chdir=/network/lustre/iss02/cohen/data/Annahita/LPC/ACSR/Decoder/code/
+#SBATCH --chdir=/network/lustre/iss02/cohen/data/Annahita/ACSR/code
 #SBATCH --output=logs/%j.out.log
 #SBATCH --error=logs/%j.err.log
-#SBATCH --job-name=test
+#SBATCH --job-name=acsr
 
 export PATH="/network/lustre/iss02/home/annahita.sarre/anaconda3/envs/myenv/bin:$PATH"
 type python
@@ -19,4 +19,5 @@ path2test_videos='../stimuli/words/mp4'
 
 python predict.py --property-type position --gender $gender --cropping $cropping --path2test-videos $path2test_videos --fn-video $1 --model-type $model_type
 python predict.py --property-type shape --gender $gender --cropping $cropping --path2test-videos $path2test_videos --fn-video $1 --model-type $model_type
+python find_onsets.py --gender $gender --cropping $cropping --path2video $path2test_videos --fn-video $1 --model-type $model_type --textgrid
 python mark_video.py --gender $gender --cropping $cropping --path2video $path2test_videos --fn-video $1 --model-type $model_type --textgrid
